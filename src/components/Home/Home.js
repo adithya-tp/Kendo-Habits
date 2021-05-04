589e0f5cd3d307323481a53a1dbfe6580bbb3c49
adithya-tp <adithyatp@yahoo.com>
2021-05-04 22:21:19 +0400

    Add text to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,30 +12,32 @@ const Home = () => {
             {/* </div> */}
             <div className="home__text">
                 <div className="home__text-top">
-
+                    <div className="top__line-one">
+                        <h1>Kendo</h1>
+                    </div>
+                    <div className="top__line-two">
+                        <h1>Habits</h1>
+                    </div>
                 </div>
                 <div className="home__text-bottom">
-
+                    <div className="bottom__line-one">
+                        <h3>Building habits</h3>
+                    </div>
+                    <div className="bottom__line-two">
+                        <h3 style={{"color": "#FA5D05"}}>Gamified</h3>
+                        <h3>!</h3>
+                    </div>
                 </div>
             </div>
             <div className="auth__buttons">
+                {/* Add ripple effect for extra pizzazz */}
                 <Ripple>
                     <div className="buttons__one">
-                        <Button
-                            primary={true}
-                        >
-                            Sign-In
-                        </Button>
-                        <Button
-                            primary={true}
-                        >
-                            Register
-                        </Button>
+                        <Button primary={true}>Sign-In</Button>
+                        <Button primary={true}>Register</Button>
                     </div>
                     <div className="buttons__two">
-                        <Button>
-                            Login with Google
-                        </Button>
+                        <Button>Login with Google</Button>
                     </div>
                 </Ripple>
             </div>
