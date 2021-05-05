44d0af997fe89f83ae96e0174e2171e8e0d0004a
adithya-tp <adithyatp@yahoo.com>
2021-05-05 07:23:18 +0400

    Style changes and firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAg2-Az-wB9_y5hLz8ywcPFJk7njwnq77g",
+    authDomain: "kendo-habits.firebaseapp.com",
+    projectId: "kendo-habits",
+    storageBucket: "kendo-habits.appspot.com",
+    messagingSenderId: "853810313107",
+    appId: "1:853810313107:web:a22e99f46e804c2219da66",
+    measurementId: "G-CX80STHMGE"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.database();
+const auth = firebaseApp.auth();
+const googleAuth = firebaseApp.googleAuth();
+
+export default {db, auth, googleAuth};
