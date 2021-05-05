2c18be9b87a0f73f0f2734a05d385d53406237d4
adithya-tp <adithyatp@yahoo.com>
2021-05-05 17:04:46 +0400

    Add framer motion on back button from authentication pages

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { useHistory } from 'react-router';
 import AuthModal from '../AuthModal/AuthModal';
@@ -16,11 +17,11 @@ const Register = () => {
                     <h3>Back to Home</h3>
                 </div>
                 <div className="arrows">
-                    <svg id="more-arrows" onClick={goBackHome}>
+                    <motion.svg whileHover={{ scale: 2 }} id="more-arrows" onClick={goBackHome}>
                         <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
                         <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
                         <polygon class="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
-                    </svg>
+                    </motion.svg>
                 </div>
             </div>
             <div className="loving">
