ae312f7969d6507d645c447559a836e2fbb8201c
adithya-tp <adithyatp@yahoo.com>
2021-05-05 08:36:26 +0400

    Basic Sign In Modal Set up

---
+++ src/components/AuthModal/FormInput.js
@@ -0,0 +1,36 @@
+import { FieldWrapper } from "@progress/kendo-react-form";
+import { Input } from "@progress/kendo-react-inputs";
+import { Label, Error, Hint } from '@progress/kendo-react-labels';
+
+export const FormInput = (fieldRenderProps) => {
+    const { validationMessage, touched, label, id, valid, disabled, hint, type, optional, ...others } = fieldRenderProps;
+
+    const showValidationMessage = touched && validationMessage;
+    const showHint = !showValidationMessage && hint;
+    const hintId = showHint ? `${id}_hint` : '';
+    const errorId = showValidationMessage ? `${id}_error` : '';
+
+    return (
+        <FieldWrapper>
+            <Label editorId={id} editorValid={valid} editorDisabled={disabled} optional={optional}>{label}</Label>
+            <div className={'k-form-field-wrap'}>
+                <Input
+                    valid={valid}
+                    type={type}
+                    id={id}
+                    disabled={disabled}
+                    ariaDescribedBy={`${hintId} ${errorId}`}
+                    {...others}
+                />
+                {
+                    showHint &&
+                    <Hint id={hintId}>{hint}</Hint>
+                }
+                {
+                    showValidationMessage &&
+                    <Error id={errorId}>{validationMessage}</Error>
+                }
+            </div>
+        </FieldWrapper>
+    );
+};
--- src/components/SignInModal/SignInModal.js
+++ src/components/SignInModal/SignInModal.js
@@ -1,12 +1,51 @@
+import { Button } from '@progress/kendo-react-buttons';
 import { Dialog } from '@progress/kendo-react-dialogs';
+import { Form, Field, FormElement } from '@progress/kendo-react-form';
 import React from 'react';
+import { FormInput } from '../AuthModal/FormInput';
+import { emailValidator, passwordValidator } from '../AuthModal/validators';
 
 const SignInModal = ({ toggleMethod }) => {
     return (
         <Dialog title="Sign In" onClose={toggleMethod}>
-            <div>
-                Sign In!
-            </div>
+            <Form
+                onSubmit={() => {}}
+                render={(formRenderProps) => (
+                    <FormElement style={{width: 400}}>
+                        <Field
+                            id={'email'}
+                            name={'email'}
+                            label={'Email'}
+                            type={'email'}
+                            component={FormInput}
+                            validator={emailValidator}
+                        />
+                        <Field
+                            id={'password'}
+                            name={'password'}
+                            label={'Password'}
+                            type={'password'}
+                            component={FormInput}
+                            validator={passwordValidator}
+                        />
+                        <div className="k-form-buttons">
+                            <Button
+                                primary={true}
+                                type={'submit'}
+                                disabled={!formRenderProps.allowSubmit}
+                            >
+                                Submit
+                            </Button>
+                            <Button onClick={formRenderProps.onFormReset}>
+                                Clear
+                            </Button>
+                        </div>
+                    </FormElement>
+
+                )}
+            >
+
+            </Form>
         </Dialog>
     );
 }
