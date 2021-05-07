2671e597083dcc2edce6d9d362dae95f5764fc5f
adithya-tp <adithyatp@yahoo.com>
2021-05-07 22:35:17 +0400

    Add first draft of dialog box on clicking habit card

--- src/components/HabitCard/HabitCard.js
+++ src/components/HabitCard/HabitCard.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardHeader } from '@progress/kendo-react-layout';
+import { TextArea } from '@progress/kendo-react-labels';
+import { Dialog } from '@progress/kendo-react-dialogs';
 import './HabitCard.css';
 import { motion } from 'framer-motion';
 
 const HabitCard = ({ title }) => {
 
+    const [expandMe, setExpandMe] = useState(false);
+
+    const expandHabitCard = () => {
+        setExpandMe(!expandMe);
+    }
+
     return (
         <motion.div
             whileHover={{ scale: 1.1 }}
+            onClick={expandHabitCard}
             className="habit__card"
         >
             <Card className="habit__card">
@@ -15,6 +24,15 @@ const HabitCard = ({ title }) => {
                     {title}
                 </CardHeader>
             </Card>
+
+            {
+                expandMe && 
+                <Dialog title={title} onClose={expandHabitCard}>
+                    <h3>Description</h3>
+
+
+                </Dialog>
+            }
         </motion.div>
     );
 }
