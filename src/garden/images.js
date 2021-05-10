2b4835ada2713b29cd6157b9d5ceb90dd971895d
adithya-tp <adithyatp@yahoo.com>
2021-05-10 23:19:02 +0400

    Add new tree assets

--- src/garden/images.js
+++ src/garden/images.js
@@ -1,9 +1,17 @@
 import land_both from "../garden_assets/land_both.png";
 import land_light from "../garden_assets/land_light.png";
 import land_dark from "../garden_assets/land_dark.png";
+import tree_one from "../garden_assets/tree_one.png";
+import tree_two from "../garden_assets/tree_two.png";
+import tree_three from "../garden_assets/tree_three.png";
+import tree_four from "../garden_assets/tree_four.png";
 
 export {
     land_both,
     land_light,
-    land_dark
+    land_dark,
+    tree_one,
+    tree_two,
+    tree_three,
+    tree_four
 }
