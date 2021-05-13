e18700bc53095af159335aa1a337df99dcae9a44
adithya-tp <adithyatp@yahoo.com>
2021-05-13 11:23:53 +0400

    Fixed password validation discrepency

--- src/components/AuthModal/validators.js
+++ src/components/AuthModal/validators.js
@@ -4,7 +4,7 @@ export const emailValidator = (value) => !value ?
     "Email field is required." :
     (emailRegex.test(value) ? "" : "Email is not in a valid format.");
 
-export const passwordValidator = (value) => value && value.length > 8 ? '' : 'Password must be at least 8 symbols.';
+export const passwordValidator = (value) => value && value.length >= 8 ? '' : 'Password must be at least 8 symbols.';
 
 export const nameValidator = (value) => !value ?
     "Please enter your name." :
