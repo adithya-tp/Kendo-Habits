e200e53363a9a74f4e7bf01a821ed2a87e95c2cb
adithya-tp <adithyatp@yahoo.com>
2021-05-15 13:15:08 +0400

    Update streak in real time and display actual habit streak

--- src/components/StreakTile/StreakTile.js
+++ src/components/StreakTile/StreakTile.js
@@ -2,9 +2,18 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import './StreakTile.css';
 
-const StreakTile = ( {color} ) => {
+const StreakTile = ( {color, h, w} ) => {
     return (
-        <motion.div whileHover={{ scale: 1.1 }} className="streak__tile" style={{ backgroundColor: color}}></motion.div>
+        <motion.div 
+            whileHover={{ scale: 1.1 }} 
+            className="streak__tile" 
+            style={{ 
+                backgroundColor: color, 
+                height: h, 
+                width: w}}
+        >
+            
+        </motion.div>
     );
 }
 
--- src/components/Streaks/Streaks.js
+++ src/components/Streaks/Streaks.js
@@ -9,10 +9,14 @@ import { useHistory } from 'react-router';
 
 function createHabitStreaks(data) {
     let cols = [];
-    for(let i = 0; i < data.length * data[0].streak.length; i++) {
-        let clr;
-        clr = data[Math.floor(i / 7)].streak[i % 7] ? data[Math.floor(i / 7)].col : "#e0e0e0";
-        cols.push(<StreakTile key={i} color={clr} />)
+
+    if(data.length) {
+        let div = data[0].streak.length;
+        for(let i = 0; i < data.length * data[0].streak.length; i++) {
+            let clr;
+            clr = data[Math.floor(i / div)].streak[i % div] ? data[Math.floor(i / div)].col : "#e0e0e0";
+            cols.push(<StreakTile h={Math.max(10, 100 - 2 * div)} w={Math.max(10, 100 - 2 * div)} key={i} color={clr} />)
+        }
     }
     return cols;
 };
@@ -21,15 +25,38 @@ const Streaks = () => {
 
     const [appbarDisplay, setAppbarDisplay] = useState('');
     const [currentUser, setCurrentUser] = useState();
+    const [habitsData, setHabitsData] = useState([]);
     const history = useHistory();
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((authUser) => {
             if(authUser) {
                 setCurrentUser(authUser);
-
                 setAppbarDisplay(authUser.displayName);
                 
+                db.collection('users')
+                .doc(authUser.uid)
+                .collection('dailyHabits')
+                .orderBy('timestamp', 'desc')
+                .onSnapshot(snapshot => {
+                    setHabitsData(
+                        snapshot.docs.map(
+                            doc => 
+                            {
+                                var last_seven = doc.data().habitHistory;
+                                last_seven = [...last_seven]
+                                last_seven = last_seven.slice(Math.max(last_seven.length - 7, 0));
+
+                                return({
+                                    id: doc.id,
+                                    habit: doc.data().habit,
+                                    col: "#" + doc.data().habitCol,
+                                    streak: last_seven
+                                });
+                            }
+                        )
+                    )
+                });
             } else {
                 history.push('/');
             }
@@ -48,25 +75,6 @@ const Streaks = () => {
         setHabitsDatePicker({ value: event.target.value })
     }
 
-    const [habitsData, setHabitsData] = useState([
-        {
-            col: "#ff0000",
-            streak: [1, 1, 1, 1, 0, 0, 0]
-        },
-        {
-            col: "#00A3FF",
-            streak: [1, 0, 1, 1, 1, 1, 1]
-        },
-        {
-            col: "#00FFB2",
-            streak: [1, 1, 1, 1, 1, 1, 1]
-        },
-        {
-            col: "#F54CB1",
-            streak: [0, 0, 0, 1, 1, 0, 1]
-        },
-    ]);
-
     return (
         <>
             <HabitAppBar userName={appbarDisplay} />
@@ -79,7 +87,14 @@ const Streaks = () => {
                         <div className="date__hover">
                             <h2>May 1</h2>
                         </div>
-                        <div className="habit__streaks">
+                        <div 
+                            className="habit__streaks"
+                            style={{
+                                display: "grid",
+                                gridTemplate: `repeat(${habitsData.length}, auto) / repeat(${habitsData.length ? habitsData[0].streak.length : 0}, auto)`,
+                                gridGap: "0.1rem"
+                            }}
+                        >
                             {
                                 createHabitStreaks(habitsData)
                             }
