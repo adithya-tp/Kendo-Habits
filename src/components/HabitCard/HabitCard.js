67137d31ad4bae04946b87d5b0dc99eab888da59
adithya-tp <adithyatp@yahoo.com>
2021-05-17 22:19:33 +0400

    Make label tags on habit card look better

--- src/components/HabitCard/HabitCard.js
+++ src/components/HabitCard/HabitCard.js
@@ -7,7 +7,7 @@ const HabitCard = ({ title, bgc, lab }) => {
 
     const lab_tags = [];
     for(let i = 0; i < lab.length; i++) {
-        lab_tags.push(<h4 style={{ opacity: 0.8, textAlign: 'center', color: "white", backgroundColor: "#3963ED", height: "10px", minWidth: "50px", maxWidth: "100px", padding: "0px 4px 10px 4px", margin: "5px 2px 0px 10px", borderRadius: "10px"}}>{lab[i]}</h4>)
+        lab_tags.push(<h4 style={{ opacity: 0.8, textAlign: 'center', color: "white", backgroundColor: "#3963ED", height: "10px", width:`${8 * lab[i].length}px`, maxWidth: "100px", padding: "0px 4px 10px 4px", margin: "5px 2px 0px 5px", borderRadius: "3px"}}>{lab[i]}</h4>)
     }
 
     return (
